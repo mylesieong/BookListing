0f1995414f1b12dfb4862e996e99210e6dbb70f5
Myles Ieong <Myles Ieong>
2016-12-05 08:15:42 +0800

    Add Custom Class: Book and change main activity to a listview

--- app/src/main/java/com/myles/udacity/booklisting/MainActivity.java
+++ app/src/main/java/com/myles/udacity/booklisting/MainActivity.java
@@ -2,6 +2,9 @@ package com.myles.udacity.booklisting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Book book= new Book(
+                "Mock book",
+                new String[]{"Myles", "Gene", "Anderson"},
+                "ORelly",
+                new Date(20161022)
+        );
+        Log.v("Myles Debug", book.toString());
     }
 }
